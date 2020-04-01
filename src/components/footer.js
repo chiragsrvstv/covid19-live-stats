5f1f48d34a092a2277095b5c66137f9eb23c1465
chiragsrvstv <chirag.srivastava@live.com>
2020-04-01 18:03:16 +0530

    footer

---
+++ src/components/footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const footer = () => {
+  return (
+    <footer className="ui borderless inverted menu">
+      {" "}
+      <div className="item footer">
+        <p>
+          Developed with{" "}
+          <span role="img" aria-label="heart">
+            {" "}
+            ❤️{" "}
+          </span>{" "}
+          by <a href="https://chiragsrvstv.codes"> @Chirag </a>
+        </p>
+      </div>{" "}
+    </footer>
+  );
+};
+
+export default footer;
